e0753be129f441b5062adecd5250e5f749fe2917
Yannis Pagès <yannis@poool.fr>
2025-03-28 17:12:26 +0100

    fix(deps): bump several deps

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,11 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
+config.transformer.minifierConfig = {
+  compress: {
+    drop_console: true,
+    drop_debugger: true,
+  },
+};
+
 module.exports = withNativeWind(config, { input: './global.css' });
